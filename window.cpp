ef7627a5afec58412c43d086975d4922d22e195c
Balalarga <lightman03@mail.ru>
2020-07-08 13:09:52 +0300

    Add glm

--- window.cpp
+++ window.cpp
@@ -4,14 +4,40 @@ using std::cout;
 
 Window::Window(const char* title)
 {
+    m_width = 640;
+    m_height = 640;
+    m_title = title;
     m_window = SDL_CreateWindow(title,
                                 SDL_WINDOWPOS_CENTERED,
                                 SDL_WINDOWPOS_CENTERED,
-                                640, 480,
-                                SDL_WINDOW_HIDDEN);
+                                m_width, m_height,
+                                SDL_WINDOW_HIDDEN | SDL_WINDOW_OPENGL);
     if(!m_window){
         cout<<"Window couldn't be created\n";
         return;
     }
     m_glContext = SDL_GL_CreateContext(m_window);
+    m_opened = false;
+}
+
+void Window::clear(){
+    glClearColor(m_clearColor.x, m_clearColor.y, m_clearColor.z, m_clearColor.w);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void Window::swap(){
+    SDL_GL_SwapWindow(m_window);
+}
+
+bool Window::isOpen()
+{
+    return m_opened;
+}
+
+void Window::setClearColor(const glm::vec4& color){
+    m_clearColor = color;
+}
+
+Window::~Window()
+{
 }
--- window.h
+++ window.h
@@ -2,21 +2,37 @@
 #define WINDOW_H
 
 #include <SDL2/SDL.h>
+#include <glm/vec4.hpp>
+#include <glm/vec2.hpp>
+#include <GLEW/glew.h>
+
 #include <string>
+#include <memory>
 using std::string;
+
+using SDLWindowPtr = std::unique_ptr<SDL_Window, void(*)(SDL_Window*)>;
+using SDLGLContextPtr = std::unique_ptr<SDL_GLContext, void(*)(SDL_GLContext)>;
 
 class Window
 {
 public:
     Window(const char *title);
+    ~Window();
+    void swap();
+    bool isOpen();
+    void clear();
+    void setClearColor(const glm::vec4& color);
 
 private:
-    SDL_Window* m_window;
+
+    SDLWindowPtr m_window(nullptr, SDL_DestroyWindow);
     SDL_GLContext m_glContext;
+    glm::vec4 m_clearColor;
 
     string m_title;
     unsigned int m_width;
     unsigned int m_height;
+    bool m_opened;
 };
 
 #endif // WINDOW_H
